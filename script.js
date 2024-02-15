e73238be86abfa3c03e3f23d2224514b56acb9b0
Ammara Arshad <159526370+Amara-Arshad@users.noreply.github.com>
2024-02-14 22:27:21 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+// const nav = document.querySelector("nav");
+
+// window.addEventListener("scroll", function(){
+//     nav.classList.toggle("sticky",this.window.scrollY > 0);
+// })
+
+// let menu = document.querySelector('#menu-icon');
+// let navbar = document.querySelector('.navbar');
+
+// menu.onclick = () => {
+//     menu.classList.toggle('bx-x');
+//     navbar.classList.toggle('open');
+// }
